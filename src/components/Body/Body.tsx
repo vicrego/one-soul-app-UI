2bccac4173cfbb0e400f7ddffff7a82a139ce133
Victor Baptista do Rego <victor@Victors-MacBook-Pro.local>
2025-06-21 20:48:57 +0100

    firstNameEntry implemented + minor changes

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,15 +1,17 @@
-import { Link as ChakraLink, LinkProps, /*Button*/ Box, Flex, HStack, Heading, MenuItemOption, Stack, Text, useColorModeValue, ButtonGroup, extendTheme, VStack, Button} from '@chakra-ui/react';
+import { Link as ChakraLink, LinkProps, /*Button*/ Box, Flex, HStack, Heading, MenuItemOption, Stack, Text, useColorModeValue, ButtonGroup, extendTheme, VStack, useDisclosure} from '@chakra-ui/react';
 import { Link as ReactRouterLink, useLocation } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, useMediaQuery } from '@mui/material';
+import { Button, CardActionArea, Modal, useMediaQuery } from '@mui/material';
 import Layoult from '../Layoult/Layoult';
 import { useEffect, useMemo, useState } from 'react';
 import { red, yellow } from '@mui/material/colors';
 import Chapter from './content/Chapter';
 import { useAuth } from '../../provider/authProvider';
+import axios from 'axios';
+
 
 
 
@@ -19,9 +21,7 @@ const Body = (props: any) => {
   then it comes from Home Link, therefore useLocation is used to handle
   the props */ 
   
-
-  const { userData } = useAuth();
-  console.log("userData", userData);
+  const {userData} = useAuth() as { userData: { data?: { username?: string, email?: string } } };
   
   if(!props.loading === true){
     const location = useLocation();
@@ -31,6 +31,55 @@ const Body = (props: any) => {
   const [imageLoaded, setImageLoaded] = useState(false);
   const isMediumHeight = useMediaQuery('(max-height: 500px)');
   const [isComponentVisible, setIsComponentVisible] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  
+  let getFirstimeEntryUrl: string;
+  let firstimeEntryUrl: string;
+  
+  if(import.meta.env.VITE_ENV === "development"){
+    getFirstimeEntryUrl = `${import.meta.env.VITE_API_URL_DEVELOPMENT}/user/getFirstTimeEntry`;
+    firstimeEntryUrl = `${import.meta.env.VITE_API_URL_DEVELOPMENT}/user/firstTimeEntry`;
+  } else {
+    getFirstimeEntryUrl = `${import.meta.env.VITE_API_URL_PRODUCTION}/user/getFirstTimeEntry`;
+    firstimeEntryUrl = `${import.meta.env.VITE_API_URL_PRODUCTION}/user/firstTimeEntry`;
+  }
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      withCredentials: true,
+      url: getFirstimeEntryUrl
+    }).then((res) => {
+      if(res.data.firstTimeEntry){
+        axios({
+          method: "POST",
+          withCredentials: true,
+          url: firstimeEntryUrl
+        }).then((res) => {
+          setOpen(true);    
+        }).catch(err => {
+          console.log("err", err);
+        });
+      } 
+    }).catch(err => {
+      console.log("err", err);
+    });
+    
+  }, [])
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    background: 'white',
+    color: 'black',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   return ( 
     <Layoult props={props.props} >  
@@ -43,6 +92,31 @@ const Body = (props: any) => {
         height={isMediumHeight ? "76vh" : "87vh"}
         overflowY={/*isMediumHeight ? "hidden" :*/ "auto"}
       >
+        <Modal
+          open={open}
+          //onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Welcome, {userData?.data?.username}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Your journey shall begin!
+            </Typography>
+            <Flex>
+              <Button variant="contained" sx={{marginLeft: "auto"}} 
+                onClick={() => {
+                  setOpen(false);
+                  //setFirstTimeEntry(false); 
+                }}
+              >
+                I'm Ready!
+              </Button>
+            </Flex>
+          </Box>
+        </Modal>
         {props?.props.courses?.map((course: any, i: any) => (
           <Box
             width={isMediumHeight ? "180px" : "16rem"}     
